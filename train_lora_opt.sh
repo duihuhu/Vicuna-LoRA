66b397a04b04208bb0c64ed1619cf5339f4adc13
duihuhu <duihuhu@163.com>
2023-11-17 10:43:59 +0800

    debug add opt fine tune py

--- train_lora_opt.sh
+++ train_lora_opt.sh
@@ -1,5 +1,5 @@
 deepspeed --include localhost:0 ./train_lora_opt.py \
-  --model_name_or_path /workspace/Sequence-Scheduling/ckpts/opt-125m \
+  --model_name_or_path /workspace/Sequence-Scheduling/ckpts/opt-6.7b \
   --lora_r 8 \
   --lora_alpha 16 \
   --lora_dropout 0.05 \
